ae5bdd1c07cb421b5edb162de8ef76e52a7195d2
C5m7b4 <c5m7b4@gmail.com>
2023-12-10 21:38:09 -0600

    add arrayDiffsSequence

--- packages/runtime/__tests__/arrays.test.js
+++ packages/runtime/__tests__/arrays.test.js
@@ -1,7 +1,25 @@
-import { withoutNulls } from "../src/utils/arrays";
+import { withoutNulls, arraysDiff } from "../src/utils/arrays";
 import { expect, test } from "vitest";
 
 test("withoutNulls", () => {
   const arr = [1, 2, "hello", null, undefined];
   expect(withoutNulls(arr)).toEqual([1, 2, "hello"]);
+});
+
+test("arraysDiff added", () => {
+  const arr1 = [1, 2, 3];
+  const arr2 = [1, 2, 3, 4];
+  expect(arraysDiff(arr1, arr2)).toEqual({
+    added: [4],
+    removed: [],
+  });
+});
+
+test("arraysDiff removed", () => {
+  const arr1 = [1, 2, 3, 4];
+  const arr2 = [1, 2, 3];
+  expect(arraysDiff(arr1, arr2)).toEqual({
+    added: [],
+    removed: [4],
+  });
 });
